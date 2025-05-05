baf23b75cd48c5fa1ceffc325ad315dfb4b70679
Rpanjiyar <95167441+Rpanjiyar@users.noreply.github.com>
2025-05-05 18:08:04 +0530

    Create cucumber.js

---
+++ config/cucumber.js
@@ -0,0 +1,25 @@
+module.exports = {
+        default: {
+            tags: process.env.npm_config_TAGS || "",
+            formatOptions: {
+                snippetInterface: "async-await"
+            },
+            paths: [
+                "src/test/features/*.feature"
+            ],
+            publishQuite: true,
+            dryRun: false,
+            require: [
+                "src/test/steps_definitions/*.ts",
+                "src/test/hooks/hooks.ts"
+            ],
+            format: [
+                "html:test-results/cucumber-report.html",
+                "json:test-results/cucumber-report.json",
+                "junit:test-results/cucumber-report.xml"
+            ],
+            requireModule: [
+                "ts-node/register"
+            ]
+        }
+}
